9c77fe0f8a76c579e78dee3a3f091e9278924006
Elizabeth Fuentes <eliaws@amazon.com>
2023-03-06 17:39:29 -0300

    App_auth

---
+++ app-recomendador/src/AppTopNavigation_auth.js
@@ -0,0 +1,140 @@
+import * as React from "react";
+import TopNavigation from "@cloudscape-design/components/top-navigation";
+import Modal from "@cloudscape-design/components/modal";
+import Box from "@cloudscape-design/components/box";
+import Button from "@cloudscape-design/components/button";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+
+import Input from "@cloudscape-design/components/input";
+import { Auth } from 'aws-amplify';
+
+export default (props) => {
+
+  const [visible, setVisible] = React.useState(false);
+  const [user_id, setUser_id] = React.useState((window.localStorage.getItem('user_id') == null ? 500000 :window.localStorage.getItem('user_id')));
+
+
+  return (
+    <div>
+      <Modal
+        onDismiss={() => setVisible(false)}
+        visible={visible}
+        closeAriaLabel="Close modal"
+        footer={
+          <Box float="right">
+            <SpaceBetween direction="horizontal" size="xs">
+              <Input
+                onChange={({ detail }) => setUser_id(detail.value)}
+                value={user_id}
+              />
+              <Button onClick={()=>{setVisible(false)}} variant="link">Cancel</Button>
+              <Button onClick={()=>{
+                setVisible(false)
+                console.log(user_id)
+                props.set_user_id(user_id)
+                
+                }} variant="primary">Ok</Button>
+            </SpaceBetween>
+          </Box>
+        }
+        header="Sobreescribir User ID"
+      >
+        Ingresa con un user ID mayor a 320.000 para conocer la experiencia de nuevos usuarios, esto debido a que el modelo se entreno con esa cantidad de usuarios.
+      </Modal>
+      <TopNavigation
+        identity={{
+          href: "/",
+          title: "Recomendador de anime",
+          logo: {
+            src:
+              "./loguito.png",
+            alt: "personalize"
+          }
+        }}
+        utilities={[
+
+          {
+            type: "menu-dropdown",
+            text: "Como crear esta aplicación",
+            description: "Recomendador de Anime",
+            iconName: "user-profile",
+            items: [
+              { id: "blog1", text: "Introducción a AI y ML", href:"https://aws.amazon.com/es/blogs/aws-spanish/introduccion-artificial-intelligence-y-machine-learning-para-desarrollares-de-aplicaciones/",external:true,
+    externalIconAriaLabel:" (opens in a new tab)"  },
+              { id: "blog2", text: "Cómo crear un modelo de recomendación basado en machine learning", href:"https://aws.amazon.com/es/blogs/aws-spanish/como-desplegar-el-modelo-recomendador-de-anime-en-una-api-rest/",external:true,
+    externalIconAriaLabel:" (opens in a new tab)" },
+              { id: "blog3", text: "Cómo desplegar el modelo recomendador de anime en una API REST", href:"https://aws.amazon.com/es/blogs/aws-spanish/como-desplegar-el-modelo-recomendador-de-anime-en-una-api-rest/",external:true,
+    externalIconAriaLabel:" (opens in a new tab)" },
+              { id: "blog4", text: "Cómo crear una aplicación web para recomendaciones personalizadas de anime en tiempo real." },
+              { id: "blog5", text: "Prox: Incorporar un pool de usuarios a tu aplicación web para recomendaciones personalizadas." },
+              { id: "blog6", text: "Prox: Analiza el comportamiento de tu aplicación web para recomendaciones personalizadas en tiempo real con un dashboard." },
+              
+              {
+                id: "support-group",
+                text: "Support",
+                items: [
+                  {
+                    id: "documentation",
+                    text: "Como crear este menu con cloudscape",
+                    href: "https://cloudscape.design/components/top-navigation/?tabId=playground",
+                    external: true,
+                    externalIconAriaLabel:
+                      " (opens in new tab)"
+                  },
+                  { id: "support", text: "Support" },
+                  {
+                    id: "feedback",
+                    text: "Feedback",
+                    href: "https://www.linkedin.com/in/lizfue/",
+                    external: true,
+                    externalIconAriaLabel:
+                      " (opens in new tab)"
+                  }
+                ]
+              },
+              { id: "signout", text: "Sign out" }
+            ]
+          },
+          
+          
+          {
+            type: "button",
+            text: "Probar con otro User ID",
+            onClick: (()=>{setVisible(true)})
+          },
+          {
+            type: "button",
+            text: "Salir",
+            onClick: (async ()=>{
+               try {
+        await Auth.signOut({ global: true });
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+              
+            })
+          },
+          {
+            type: "button",
+            text: "Amazon Personalize",
+            href: "https://aws.amazon.com/es/personalize/",
+            external: true,
+            externalIconAriaLabel: " (opens in a new tab)"
+          },
+          
+          
+
+        ]}
+        i18nStrings={{
+          searchIconAriaLabel: "Search",
+          searchDismissIconAriaLabel: "Close search",
+          overflowMenuTriggerText: "More",
+          overflowMenuTitleText: "All",
+          overflowMenuBackIconAriaLabel: "Back",
+          overflowMenuDismissIconAriaLabel: "Close menu"
+        }}
+      />
+    </div>
+
+  );
+}
---
+++ app-recomendador/src/App_auth.js
@@ -0,0 +1,96 @@
+import "@cloudscape-design/global-styles/index.css"
+import * as React from "react";
+import { useEffect } from "react";
+
+import {
+  Routes, 
+  Route, 
+  Outlet,
+  
+} from "react-router-dom";
+import AppLayout from "@cloudscape-design/components/app-layout"; 
+import Home from './Home'
+
+
+import AppTopNaigation from "./AppTopNaigation";
+import "./App.css"
+import Top_4 from "./Top_4";
+import { Topic } from "./Anime";
+
+import { Amplify, Auth } from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+import APIS from "./apis_url.js"
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsconfig)
+
+
+const App =  () => {
+  const d = new Date()
+
+
+  const [user_id, setUser_id] = React.useState((window.localStorage.getItem('user_id') == null ? 500000 :window.localStorage.getItem('user_id')));
+  const [session, setSesion] = React.useState((window.localStorage.getItem('session') == null ? "500000"+ d.getTime() :window.localStorage.getItem('session')));
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then( user => {
+      console.log(user); 
+      window.localStorage.setItem('user_id', user.username)
+      setUser_id(user.username)
+      return user.username
+    }
+  )
+  
+
+  }, []);
+
+
+  const set_user_id = (new_user_id) => {
+    const d = new Date()
+    const new_session =String(new_user_id)+d.getTime()
+
+    window.localStorage.setItem('user_id', new_user_id)
+    window.localStorage.setItem('session', new_session)
+
+    setUser_id(new_user_id)
+    setSesion(new_session)
+    console.log("user_id!")
+  }
+  return (
+    <Routes>
+      <Route path="/" element = {<Layout  set_user_id = {set_user_id} />}>
+        <Route path="/:MAL_ID" element={<Topic APIS={APIS}  user_id={user_id} session={session}/>}>
+        </Route>
+        <Route index element = {<Home  user_id={user_id} APIS={APIS}/>}/>
+        <Route path="/" element = {<Top_4/>}/>
+      </Route>
+    </Routes>
+
+  );
+}
+
+const Layout = (props) => [
+  
+  <AppTopNaigation key={1} set_user_id= {props.set_user_id} />,
+  <AppLayout key={2}
+    headerSelector = "#h"
+    toolsHide = {true}
+    disableContentPaddings = {true}
+    navigationHide = {true}
+    footerSelector="#f"
+    content = {<Outlet/>}
+    
+    />,
+    <div id="f" key={3} className='App-footer'>
+      @2023 Elizabeth Fuentes Leone | AWS Developer Advocate | All Rights Reserved
+
+    </div>
+
+]
+
+const Nomatch = () => <div> Nada por aca </div>
+
+export default withAuthenticator(App)
